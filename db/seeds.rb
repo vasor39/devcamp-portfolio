2ea6740a33937f5801faac5458277508fd2f35d5
vasor <vasonaruto@gmail.com>
2018-09-18 16:11:05 +0300

    seeds skill etc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Whatetnibjrkf foefjnf ojldnsflkjsfe skj dsoijds
+    jkdnsfklmds sedfijdknmdsfoiklmdsf sfdkjldnmfdskjlkm,dsfvc dsfvc
+    ljfndm,dgjlkmfds fdsknmfd dfsskjnmfds jndsfg"
+  )
+end
+
+puts "10 blogs created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "#{skill} Skill",
+    percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My great service",
+    body: "asdfgdsafg fds fedsg jknfdsgf dslfkgnmfsd
+    lkdsmfdgfkmfd klmfsddgbf klmfdsndg fdsdmgflknsdf",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x150"
+  )
+end
+puts "9 portfolio items created"
