61f8b88e056e6f8137a86cf826ccf1e4e9653396
vasor <vasonaruto@gmail.com>
2018-09-19 11:56:44 +0300

    generate friendly_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
   # get 'pages/home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
