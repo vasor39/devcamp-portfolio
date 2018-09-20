df5bfde7150d840a72b6f146903b0c7460f104e9
vasor <vasonaruto@gmail.com>
2018-09-20 14:39:48 +0300

    refactored params section in portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,9 +13,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(
-      :title, :subtitle, :body, technologies_attributes: [:name]
-      ))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -37,9 +35,7 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(
-        :title, :subtitle, :body
-        ))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
         format.json { render :show, status: :ok, location: @portfolio_item }
       else
@@ -66,4 +62,12 @@ class PortfoliosController < ApplicationController
       }
     end
   end
+
+  private
+    def portfolio_params
+      params.require(:portfolio).permit(
+        :title, :subtitle, :body, technologies_attributes: [:name]
+      )
+    end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ Bundler.require(*Rails.groups)
 
 module DevcampPortfolio
   class Application < Rails::Application
+    # config.action_controller.permit_all_parameters = true
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
