fce0d43fa9d498c38f3b9d0650fe7ec48b763abc
vasor <vasonaruto@gmail.com>
2018-09-21 11:11:31 +0300

    implemented origin view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
       (link_to "Register", new_user_registration_path)
     end
   end
+
+  def sample_helper
+    content_tag(:div, "My content", class: "my-class")
+  end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from  #{session[:source]}"
+      greeting += " and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
 end
