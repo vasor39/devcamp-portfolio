619e86a058b2acd3ad8005f1cf801dd81e78567c
vasor <vasonaruto@gmail.com>
2018-09-25 11:50:35 +0300

    apply authorization in sorting portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,6 @@
 class PortfoliosController < ApplicationController
   layout 'portfolio'
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
 
   def index
     @portfolio_items = Portfolio.by_position
