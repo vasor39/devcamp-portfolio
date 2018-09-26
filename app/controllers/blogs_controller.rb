601eb167d300ce7aba01fef155ac4241b83c79f4
vasor <vasonaruto@gmail.com>
2018-09-26 13:06:16 +0300

    implemented the non empty blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:toggle_status, :update, :create, :destroy, :edit]
+
   layout "blog"
   access all: [:show, :index],
          user: {
@@ -99,5 +101,9 @@ class BlogsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
       params.require(:blog).permit(:title, :body, :topic_id)
+    end
+
+    def set_sidebar_topics
+      @side_bar_topics = Topic.with_blogs
     end
 end
